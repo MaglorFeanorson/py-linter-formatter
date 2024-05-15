01464b1f0461927fa2999d8b62eae4383a001129
MaglorFeanorson <157366510+MaglorFeanorson@users.noreply.github.com>
2024-05-15 22:15:25 +0300

    'Solution_3try'

--- app/main.py
+++ app/main.py
@@ -7,7 +7,6 @@ def format_linter_error(error: dict) -> dict:
             }
 
 
-
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
